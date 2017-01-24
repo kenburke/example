4fb37ae2926d34881a59c0e2e42acca51651a4d7
ken_burke <kennethjburkejr@gmail.com>
2017-01-23 17:34:06 -0800

    Detab algorithm testing

--- test/test_algs.py
+++ test/test_algs.py
@@ -20,26 +20,26 @@ def test_bubblesort():
     # think about: (1) does your code handle 0-element arrays without
     # failing, (2) does your code handle characters?
 
-	#duplicates test
+    #duplicates test
     x = np.array([1,2,4,0,1])  
     algs.bubblesort(x)
     assert np.array_equal(x, np.array([0,1,1,2,4]))
 
-	#test empty vector
-	x = np.array([])
-	algs.bubblesort(x)
-	assert np.array_equal(x, np.array([]))
-	
-	#test single element vector
-	x = np.array([2])
-	algs.bubblesort(x)
-	assert np.array_equal(x, np.array([2]))
-	
-	#test negative values (and even size vector)
-	x = np.array([-10,12,-2,5,-7,0])
-	algs.bubblesort(x)
-	assert np.array_equal(x, np.array([-10,-7,-2,0,5,12]))
-	
+    #test empty vector
+    x = np.array([])
+    algs.bubblesort(x)
+    assert np.array_equal(x, np.array([]))
+    
+    #test single element vector
+    x = np.array([2])
+    algs.bubblesort(x)
+    assert np.array_equal(x, np.array([2]))
+    
+    #test negative values (and even size vector)
+    x = np.array([-10,12,-2,5,-7,0])
+    algs.bubblesort(x)
+    assert np.array_equal(x, np.array([-10,-7,-2,0,5,12]))
+    
 
 def test_quicksort():
 
