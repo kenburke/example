b05df09ff22ddb447abf01b026aa7a785a267dc1
ken_burke <kennethjburkejr@gmail.com>
2017-01-23 17:11:03 -0800

    Add iPython notebook

--- example/algs.py
+++ example/algs.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+assignments = 0
+conditionals = 0
+
 def pointless_sort(x):
     """
     This function always returns the same values to show how testing
@@ -8,64 +11,104 @@ def pointless_sort(x):
     return np.array([1,2,3])
 
 def bubblesort(x):
-	"""
-	Sorts numpy array `x` by swapping adjacent elements when needed.
-	Runtime goes with O(n^2).
-	"""
-	
-	print("Unsorted Input :   ", x)
-	
-	for i in range(0,np.size(x)):
-		for j in range(np.size(x),i+1,-1):
-			if x[j-1]<x[j-2]:
-				temp = x[j-1]
-				x[j-1] = x[j-2]
-				x[j-2] = temp
-				
-	print("\nSorted Output :  ", x, "\n\n")
-	
-	return x
+
+    """
+    Sorts numpy array `x` by swapping adjacent elements when needed.
+    Runtime goes with O(n^2).
+    """
+    
+    assignments = 0             # for measuring complexity
+    conditionals = 0
+      
+    for i in range(0,np.size(x)):
+        for j in range(np.size(x),i+1,-1):
+        
+            conditionals += 1
+            
+            if x[j-1]<x[j-2]:
+            
+                assignments += 3
+                temp = x[j-1]
+                x[j-1] = x[j-2]
+                x[j-2] = temp
+                   
+    return conditionals, assignments
 
 def qsPartition(x, first, last):
-	"""
-	Partition step of quicksort function.
-	Makes sure all components of one partition is less than the other.
-	Input: numpy array x, with starting and ending indeces.
-	Output: index of partition ("pivot").
-	"""
-	
-	pivotVal = x[last]		#arbitrarily call the last value the "pivot value"
-	pivotLoc = first-1		#this will move as we swap low values into start of x
-	
-	for ii in range(first,last):		# go through x (O(n))
-		if x[ii] <= pivotVal:
-			pivotLoc += 1				# if your value is less than pivot value,
-			t = x[ii]					# move the pivot location up one slot
-			x[ii] = x[pivotLoc]			# and swap x[pivotLoc] with your value ii
-			x[pivotLoc] = t
-	
-	tt = x[pivotLoc+1]					# finally, put the pivot value (which was
-	x[pivotLoc+1] = x[last]				# arbitrarily chosen as the last value) and
-	x[last] = tt						# swap it into the pivotLoc
-	
-	return (pivotLoc + 1)				# return the pivot location for quicksort
-			
-	
 
-def quicksort(x, first=0, last=-1):
+    """
+    Partition step of quicksort function.
+    Makes sure all components of one partition is less than the other.
+    Input: numpy array x, with starting and ending indeces.
+    Output: index of partition ("pivot").
+    """
+    conditionals = 0
+    assignments = 0
+    
+    assignments += 2
+    pivotVal = x[last]      #arbitrarily call the last value the "pivot value"
+    pivotLoc = first-1      #this will move as we swap low values into start of x
+    
+    for ii in range(first,last):        # go through x (O(n))
+    
+        conditionals += 1
+        if x[ii] <= pivotVal:
+            
+            assignments += 4
+            
+            pivotLoc += 1               # if your value is less than pivot value,
+            t = x[ii]                   # move the pivot location up one slot
+            x[ii] = x[pivotLoc]         # and swap x[pivotLoc] with your value ii
+            x[pivotLoc] = t
+    
+    assignments += 3
+    
+    tt = x[pivotLoc+1]                  # finally, put the pivot value (which was
+    x[pivotLoc+1] = x[last]             # arbitrarily chosen as the last value) and
+    x[last] = tt                        # swap it into the pivotLoc
+
+    return (pivotLoc + 1), conditionals, assignments # return the pivot location for quicksort
+        
+    
+
+def quicksort(x, first=None, last=None):
+
     """
     Sorts numpy array `x` from index "first" to "last"
-    	through "divide/conquer" recursive partitioning in place.
+        through "divide/conquer" recursive partitioning in place.
+        Counters are used to keep track of complexity.
     Average runtime goes with O(n*lg n), worst-case n^2.
     """
     
-    if last==-1:
-    	last = np.size(x)-1
+    assignments = 0
+    conditionals = 0
     
+    conditionals += 2
+    if first is None:
+        assignments += 1
+        first = 0
+    if last is None:
+        assignments += 1
+        last = np.size(x)-1
+        
+    conditionals += 1
     if first<last:
-    	pivot = qsPartition(x,first,last)
-    	quicksort(x,first,pivot-1)
-    	quicksort(x,pivot+1,last)
-
-    return x
-
+        assignments += 7
+        
+        pivot, c, a = qsPartition(x,first,last)
+        conditionals += c
+        assignments += a
+        c,a = quicksort(x,first,pivot-1)
+        conditionals += c
+        assignments += a
+        c,a = quicksort(x,pivot+1,last)
+        conditionals += c
+        assignments += a
+        
+    return conditionals, assignments
+    
+# print("Unsorted Input :\n", x)
+# quicksort(x)
+# print("\nSorted Output :\n", x)
+# print("\nConditionals :  ", conditionals)
+# print("Assignments :  ", assignments, "\n\n")
