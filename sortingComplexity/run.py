43c777f70308ca4b2213224424be6979ae384bb8
ken_burke <kennethjburkejr@gmail.com>
2017-01-24 19:04:51 -0800

    Preliminary testing and plotting of complexity

--- sortingComplexity/run.py
+++ sortingComplexity/run.py
@@ -3,6 +3,8 @@
 # the same directory as this one.
 
 import numpy as np
+import sys
+import matplotlib.pyplot as plt
 from .algs import quicksort, bubblesort
 
 def basic_test():
@@ -25,62 +27,176 @@ def basic_test():
     
     print("\n\tBubblesort output == Quicksort output?  ", np.array_equal(bsSorted, qsSorted))
       
-def complexity_test():
+def complexity_experiment():
     """
     Tests complexity (as defined by time, assignments and conditionals) of
     the sorting algorithms as a function of input data size.
     """
     
-    inputDataSizes = [100,200,300,400,500,600,700,800,900,1000,2000,5000]
+    inputDataSizes = [100,200,300,400,500,600]#,700,800,900,1000]
     repsPerInputSize = 100
     
-    assignments = np.zeros((len(inputDataSizes),repsPerInputSize),2)
-    conditionals = np.zeros((len(inputDataSizes),repsPerInputSize),2)
-    time = np.zeros((len(inputDataSizes),repsPerInputSize),2)
+    assignments = np.zeros((2,len(inputDataSizes),repsPerInputSize))
+    conditionals = np.zeros((2,len(inputDataSizes),repsPerInputSize))
+    time = np.zeros((2,len(inputDataSizes),repsPerInputSize))
     
     progress = 0
-    checkpoints = {'1':2,'2':4,'3':6,'4':9,'5':12,'6':15,'7':18,'8':21,'9':25,'10':50,'11':100}
-    
-    print("\n\tTesting Bubblesort Complexity:")
-    print("\n\tinputDataSizes = ", inputDataSize)
-    print("\n\trepsPerInputSize = ", repsPerInputSize,"\n")
-    update_progress(progress)
+    checkpoints = {'0':4,'1':8,'2':16,'3':24,'4':36,'5':48,'6':60,'7':72,'8':84,'9':100}
+
+    print("\n------------------------------------------------\n")        
+    print("\tTesting Bubblesort Complexity:")
+    print("\tinputDataSizes = ", inputDataSizes)
+    print("\trepsPerInputSize = ", repsPerInputSize,"\n")
 
     for i in range(0,len(inputDataSizes)):
-        
+
         #run bubblesort many times for each input size
         for j in range(0,repsPerInputSize):
             data = np.random.rand(inputDataSizes[i])
-            c, a, t = algs.bubblesort(data)
-            conditionals[i,j,0] = c
-            assignments[i,j,0] = a
-            time[i,j,0] = t
-                
-        if i in checkpoints.keys():
+            c, a, t = bubblesort(data)
+            conditionals[0,i,j] = c
+            assignments[0,i,j,] = a
+            time[0,i,j] = t
+            
+        progress = checkpoints[str(i)]
+        if str(i) in checkpoints.keys():
             update_progress(progress)
     
-    print("\n------------------------------------------------")        
-    print("\n\tTesting Quicksort Complexity:")
-    print("\n\tinputDataSizes = ", inputDataSize)
-    print("\n\trepsPerInputSize = ", repsPerInputSize,"\n")
+    print("\n------------------------------------------------\n")        
+    print("\tTesting Quicksort Complexity:")
+    print("\tinputDataSizes = ", inputDataSizes)
+    print("\trepsPerInputSize = ", repsPerInputSize,"\n")
     progress = 0
-    update_progress(progress)
 
     for i in range(0,len(inputDataSizes)):
-        
+    
         #run quicksort many times for each input size
         for j in range(0,repsPerInputSize):
             data = np.random.rand(inputDataSizes[i])
-            c, a, t = algs.quicksort(data)
-            conditionals[i,j,1] = c
-            assignments[i,j,1] = a
-            time[i,j,1] = t
-                
-        if i in checkpoints.keys():
+            c, a, t = quicksort(data)
+            conditionals[1,i,j] = c
+            assignments[1,i,j] = a
+            time[1,i,j] = t
+
+        progress = checkpoints[str(i)]    
+        if str(i) in checkpoints.keys():
             update_progress(progress)
     
-    return assignments, conditionals, time
+    print("\n------------------------------------------------\n")        
     
+    complexity = {'assign':assignments,'cond':conditionals,'time':time}
+    inputData = {'sizes':inputDataSizes,'reps':repsPerInputSize}
+    
+    return complexity, inputData
+
+def complexity_visualize(complexity, inputData):
+    
+    #first get averages and stdev of assign, cond and time for each input data size for bubblesort
+    meanBSAssign = np.average(complexity['assign'][0,:,:],axis=1)
+    meanBSCond = np.average(complexity['cond'][0,:,:],axis=1)
+    meanBSTime = np.average(complexity['time'][0,:,:],axis=1)
+    stdBSAssign = np.std(complexity['assign'][0,:,:],axis=1)
+    stdBSCond = np.std(complexity['cond'][0,:,:],axis=1)
+    stdBSTime = np.std(complexity['time'][0,:,:],axis=1)
+    
+    #now quicksort
+    meanQSAssign = np.average(complexity['assign'][1,:,:],axis=1)
+    meanQSCond = np.average(complexity['cond'][1,:,:],axis=1)
+    meanQSTime = np.average(complexity['time'][1,:,:],axis=1)
+    stdQSAssign = np.std(complexity['assign'][1,:,:],axis=1)
+    stdQSCond = np.std(complexity['cond'][1,:,:],axis=1)
+    stdQSTime = np.std(complexity['time'][1,:,:],axis=1)
+    
+    
+    fig = plt.figure(figsize=(20,10))
+    st = fig.suptitle('Runtime, Assignments, Conditionals Comparison\nBubblesort (red) vs Quicksort (blue)\n',
+        fontsize="x-large")
+    
+    #plot Runtime Differences (log scale)
+    plt.subplot(231)
+    plt.errorbar(inputData['sizes'],meanBSTime,yerr=stdBSTime,color="r")
+    plt.errorbar(inputData['sizes'],meanQSTime,yerr=stdQSTime,color="b")
+        
+    plt.yscale('log')
+    plt.ylabel('log')
+    plt.title('Runtime (seconds)', fontsize=10)
+    
+    plt.axis([0.95*min(inputData['sizes']),1.1*max(inputData['sizes']),
+        0.95*min(min(meanBSTime),min(meanQSTime)),1.3*max(max(meanBSTime),max(meanQSTime))])  
+                      
+    #plot Runtime Differences
+    plt.subplot(234)
+    plt.errorbar(inputData['sizes'],meanBSTime,yerr=stdBSTime,color="r")
+    plt.errorbar(inputData['sizes'],meanQSTime,yerr=stdQSTime,color="b")
+        
+    plt.xlabel('Input Data Size')
+    plt.ylabel('scalar')
+    
+    plt.axis([0.95*min(inputData['sizes']),1.1*max(inputData['sizes']),
+        0.95*min(min(meanBSTime),min(meanQSTime)),1.2*max(max(meanBSTime),max(meanQSTime))])  
+                  
+    #plot Assign Differences (log)
+    plt.subplot(232)
+    plt.errorbar(inputData['sizes'],meanBSAssign,yerr=stdBSAssign,color="r")
+    plt.errorbar(inputData['sizes'],meanQSAssign,yerr=stdQSAssign,color="b")
+        
+    plt.yscale('log')
+    plt.title('Assignments', fontsize=10)
+    
+    plt.axis([0.95*min(inputData['sizes']),1.1*max(inputData['sizes']),
+        0.95*min(min(meanBSAssign),min(meanQSAssign)),1.2*max(max(meanBSAssign),max(meanQSAssign))])  
+                  
+    #plot Assign Differences
+    plt.subplot(235)
+    plt.errorbar(inputData['sizes'],meanBSAssign,yerr=stdBSAssign,color="r")
+    plt.errorbar(inputData['sizes'],meanQSAssign,yerr=stdQSAssign,color="b")
+        
+    plt.xlabel('Input Data Size')
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+
+    plt.axis([0.95*min(inputData['sizes']),1.1*max(inputData['sizes']),
+        0.95*min(min(meanBSAssign),min(meanQSAssign)),1.2*max(max(meanBSAssign),max(meanQSAssign))])  
+        
+    #plot Conditionals Differences (log)
+    plt.subplot(233)
+    plt.errorbar(inputData['sizes'],meanBSCond,yerr=stdBSCond, color="r")
+    plt.errorbar(inputData['sizes'],meanQSCond,yerr=stdQSCond,color="b")
+        
+    plt.yscale('log')
+    plt.title('Conditionals', fontsize=10)
+
+    plt.axis([0.95*min(inputData['sizes']),1.1*max(inputData['sizes']),
+        0.95*min(min(meanBSCond),min(meanQSCond)),1.2*max(max(meanBSCond),max(meanQSCond))])  
+                  
+    #plot Conditionals Differences
+    plt.subplot(236)
+    plt.errorbar(inputData['sizes'],meanBSCond,yerr=stdBSCond, color="r")
+    plt.errorbar(inputData['sizes'],meanQSCond,yerr=stdQSCond,color="b")
+        
+    plt.xlabel('Input Data Size')
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+
+    plt.axis([0.95*min(inputData['sizes']),1.1*max(inputData['sizes']),
+        0.95*min(min(meanBSCond),min(meanQSCond)),1.2*max(max(meanBSCond),max(meanQSCond))])  
+
+    #show and save                  
+    plt.show()
+
+    st.set_y(0.95)
+    fig.subplots_adjust(top=0.85)
+    
+    fig.savefig("test.png")
+    
+    return
+
+def complexity_test():
+
+    complexity, inputData = complexity_experiment()
+    
+    complexity_visualize(complexity, inputData)
+
 def update_progress(progress):
-    print('\r[{0}] {1}%'.format('#'*(progress/10),progress,end='')
+    sys.stdout.write('\r')
+    sys.stdout.write('[{0}{1}] {2}%'.format('#'*(progress//2),' '*(50-progress//2),progress))    
+    sys.stdout.flush()
     
